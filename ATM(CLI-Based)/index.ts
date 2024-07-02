7d347014e889d06a7d521b4536fe3878a9f69bb3
Muhammad Shayan Faisal <muhammadshayanfaisal@gmail.com>
2024-07-03 00:05:50 +0500

    ATM-CLI-BASED-PROJECT

---
+++ ATM(CLI-Based)/index.ts
@@ -0,0 +1,66 @@
+#! /usr/bin/env node
+import inquirer from"inquirer"
+let mybalance=10000;
+console.log("Your balance is: ",mybalance);
+let myPin=8041;
+ let pinans=await inquirer.prompt(
+    [
+        {
+            name:"q1",
+            message:"Enter your pin",
+            type:"number"
+        }
+    ]
+ );
+if(pinans.q1==myPin){
+    console.log("Correct Pin code");
+    let operationans=await inquirer.prompt(
+        [
+            {
+                name:"operation",
+                message:"Select option",
+                type:"list",
+                choices:["withdraw","check balance","fastcheque"]
+            }
+        ]
+    );
+    if(operationans.operation==="withdraw"){
+        let amountans=await inquirer.prompt(
+            [
+                {
+                    name:"amount",
+                    message:"Enter the amount you want to withdraw",
+                    type:"input",
+                    validate: (input) => {
+                        const numberInput = Number(input);
+                        if (isNaN(input) || numberInput <= 0) {
+                            return ("Amount must be a number and cannot be zero or negative");
+                        }
+                        return true;
+                    },
+                }
+            ]
+        );
+        mybalance-=amountans.amount;
+        console.log(`Your Remaining balance is: ${mybalance}`);
+    }
+    else if(operationans.operation==="check balance"){
+        console.log(`You current balance is: ${mybalance}`);
+    }
+    else if(operationans.operation==="fastcheque"){
+        let ch=await inquirer.prompt(
+            [
+                {
+                    name:"choic",
+                    message:"Please select below any of the payment if you want",
+                    type:"list",
+                    choices:[1000,2000,5000,10000]
+                }
+            ]
+        );
+        mybalance-=ch.choic;
+        console.log(`Your Remaining balance is: ${mybalance}`);
+    }
+}
+else
+console.log("Incorrect pin number"); 
