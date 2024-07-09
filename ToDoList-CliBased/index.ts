a9aade68d2ba034d204a6b2f6293be93d805a155
Muhammad Shayan Faisal <muhammadshayanfaisal@gmail.com>
2024-07-09 18:09:15 +0500

    ToDoList-CliBased

---
+++ ToDoList-CliBased/index.ts
@@ -0,0 +1,75 @@
+#! /usr/bin/env node
+import inquirer from"inquirer"
+let todos=[];
+let condition =true;
+while(condition){
+    let todoQuestions=await inquirer.prompt([
+            {
+                name:"firstquestion",
+                type:"input",
+                message:"What do you want to add in your todos?",
+                validate: (input) => {
+                    const stringInput = String(input);
+                    if (stringInput===""|| stringInput===" ") {
+                        return ("ToDo cannot be empty");
+                    }
+                    return true;
+                },
+            },
+            {
+                name:"update",
+                type:"confirm",
+                message:"Would you like to update anything in your todo",
+                default:"false"
+            },
+        ]
+     );
+     if(todoQuestions.update!==true){
+        todos.push(todoQuestions.firstquestion);
+        console.log(todoQuestions.firstquestion);
+     }
+     else{
+        let a=await inquirer.prompt(
+            [
+                {
+                    name:"updatetodo",
+                    type:"input",
+                    message:"Update your todo",
+                }
+            ]
+        );
+        console.log("Updating your todo....");
+        todos.push(a.updatetodo);
+        console.log(a.updatetodo);
+     }
+     console.log(todos);
+     let b=await inquirer.prompt(
+        [
+            {
+                name:"delete",
+                type:"confirm",
+                message:"Would you like to delete your todo's",
+                default:"false"
+            },
+        ]
+     );
+     if(b.delete===true){
+        todos.pop();
+     }
+    
+     console.log(todos);
+     let c=await inquirer.prompt(
+        [
+            {
+                name:"secondquestion",
+                type:"confirm",
+                message:"Would you like to add more in your todo's",
+                default:"true"
+            },
+        ]
+     )
+     condition=c.secondquestion;
+};
+
+
+
